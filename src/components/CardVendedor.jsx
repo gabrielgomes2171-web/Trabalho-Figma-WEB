db381cc03278dc7d2c6e605e835f5cb6d2f7fa17
Jorbson <jorbsonsilva93@gmail.com>
2025-11-14 21:55:08 -0300

    imagens e cardvendedor

--- src/components/CardVendedor.jsx
+++ src/components/CardVendedor.jsx
@@ -1,17 +1,42 @@
-const CardVendedor = ({vendedorData}) =>{
-    const {nome, localizacao, rating, descricao, pedido, fundada} = vendedorData;
-    return ( <><div className="card vendedor"></div>
-    <div className="card media"></div>
-    <div className="card content">
-        <h3>{nome}</h3>
-        <h2>{localizacao}</h2>
-        <h2>{rating}</h2>
-        <h2>{descricao}</h2>
-        <h2>{pedido}</h2>
-        <h2>{fundada}</h2>
+import Link from "next/link";
+
+const CardVendedor = ({vendedor}) =>{
+    const {nome, local, nota, avaliacoes, produtos, seguidores, fundada, minPedido, 
+        especialidades, qualidades} = vendedor;
+    return ( 
+    <div className="card-vendedor">
+        {premium && <span className="premium-tag">Qualidade Premium</span>}
+
+        <div className="card-image"> 
+            < img src={image || '/placeholder.webp'}
+                  alt={'Loja ${nome}'}
+                  className="card-image" />
+        </div>
+
+        <div className="card-content">
+            <h3 className="vendedor-name">{nome}</h3>
+            <p className="vendedor-local">{local}</p>
+            <div className="vendedor-status">
+                <div className="status-itens">
+                    <span className="rating">⭐</span>
+                    <span className="status-valor">{nota}</span>
+                </div>
+            </div>       
+        </div>
+        <div className="vendedor-tags">
+            {especialidades.map(tag =>(
+                <span key={tag} className="especialidade-tag">{tag}</span>
+            ))}
+        </div>
+        <div className="card-acao">
+            <Link href={'/loja/${id}'} passHref>
+                <a className="botao-ver-loja">Ver Loja</a>
+            </Link>
+            <button className="botao-seguir">Seguir</button>
+        </div>
         
-    </div></>
-    
+
+     </div>   
 
 
 
