72e13eb473a7e468eb2ec18a6f4f66a7759f557a
Jorbson <jorbsonsilva93@gmail.com>
2025-12-01 12:51:45 -0300

    pagina final vendedor

--- src/app/vendedores/page.jsx
+++ src/app/vendedores/page.jsx
@@ -1,19 +1,36 @@
 import Header from "@/components/header/Header";
 import Footer from "@/components/footer/Footer";
+import Button from "@components/Button";
 import CardVendedor from "@/components/vendedor/CardVendedor";
+import VenderSearchBar from "@components/vendedor/VenderSearchBar";
 import { vendedoresData } from "../data/vendedores";
 import styles from "./vendedores.module.css";
+
 
 export default function VendedoresPage(){
     return (
         <main>
             <Header/>
-            <h1>Pagina dos Vendedores</h1>
+
+            <div className={styles.pageWrapper}>
+
+            <VenderSearchBar/>
             
             <div className={styles.cardGrid}>{vendedoresData.map((vendedorItem)=>(
                 <CardVendedor className= {styles.cardGrid} key={vendedorItem.id} vendedorData={vendedorItem}/>))}
 
-            </div>  
+            </div>
+             <div className={styles.pagination}>
+                        <Button className={styles.prevNextButton}>Anterior</Button>
+                        <span className={styles.pageNumber}>1</span>
+                        <span className={styles.pageNumber}>2</span>
+                        <span className={styles.pageNumber}>3</span>
+                        <span className={styles.pageNumber}>4</span>
+                        <Button className={styles.prevNextButton}>Próximo</Button>
+                    </div>
+
+            </div>
+
             <Footer/>        
 
         </main>
